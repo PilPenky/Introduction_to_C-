6ec843fb7c6ce956aa6e2ee7988aece154f25167
Artur Pilipenko <apg-a@mail.ru>
2022-12-16 13:45:57 +0200

    ДЗ_1

---
+++ Homework/Homework_Lesson1/Exercise 1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает два целых числа и выдает, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1==number2)
+    {
+        Console.WriteLine($"Числа равны.");
+    }
+
+if(number1 > number2)
+    {
+        Console.WriteLine($"Число {number1} больше, чем {number2}.");
+    }
+if(number1 < number2)
+    {
+        Console.WriteLine($"Число {number2} больше, чем {number1}.");
+    }
