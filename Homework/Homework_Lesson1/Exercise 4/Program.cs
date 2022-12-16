3659a527dee6e7a6397388bce38c71c035155f17
Artur Pilipenko <apg-a@mail.ru>
2022-12-16 15:00:28 +0200

    ДЗ_4

---
+++ Homework/Homework_Lesson1/Exercise 4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая на вход принимает целое число (N), 
+а на выходе показывает все чётные числа от 1 до N.*/
+
+Console.WriteLine("Введите целое число от 1 до ...N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("---");
+
+int startNumber = 1;
+
+while(startNumber != number+1)
+    {
+        if(startNumber%2 == 0)
+            {
+                Console.WriteLine(startNumber);
+                startNumber++;
+            }
+        else
+            {
+                startNumber++;
+            }
+    }
