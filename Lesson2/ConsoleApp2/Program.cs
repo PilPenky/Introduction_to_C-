6b605381cedfa135022b7c41696be22e51b982bc
Artur Pilipenko <apg-a@mail.ru>
2022-12-19 20:25:43 +0200

    Добавил второе задание

---
+++ Lesson2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая выводит случайное трехзначное 
+число и удаляет вторую цифру этого числа.
+*/
+
+//int number = new Random().Next(100, 1000);
+int number = 999;
+
+int thirdDigit = number % 10; //3
+
+int firstDigit = (number / 100); 
+
+int result = firstDigit * 10 + thirdDigit;
+
+Console.WriteLine(number);
+Console.Write(result);
---
+++ Lesson2/ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+Если число 2 не кратно числу 1, то программа выводит остаток от деления. */
+
+Console.WriteLine("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 == 0)
+{
+    Console.WriteLine("Второе число не может быть нулем");
+}
+else
+{
+    int rest = number % number2;
+    if (rest == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    if (rest != 0)
+    {
+        Console.WriteLine($"Не кратно, {rest}");
+    }
+}
+
+
