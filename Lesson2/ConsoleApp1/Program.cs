d707759be62012c90dab1dfb963fc9b8b4716b81
Artur Pilipenko <apg-a@mail.ru>
2022-12-19 19:49:16 +0200

    Добавил папку Lesson2

---
+++ Lesson2/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит случайное число 
+//из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int number = new Random().Next(10, 99);
+
+//Первая цифра числа
+int firstDigit = number / 10;
+
+//Вторая цифра числа
+int secondDigit = number % 10;
+
+Console.WriteLine($"Случайное число: {number}");
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа: {firstDigit}");
+}
+
+if (secondDigit > firstDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа: {secondDigit}");
+}
+
+if (secondDigit == firstDigit)
+{
+    Console.WriteLine("Цифры в числе равны");
+}
