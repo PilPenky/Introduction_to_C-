f71729944a7f01fb54996162035a13fc74f600f1
Artur Pilipenko <apg-a@mail.ru>
2022-12-20 14:40:49 +0200

    ДЗ_1

---
+++ Homework/Homework_Lesson2/Exercise 1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if((number > 999) || (number < 100))
+{
+    Console.WriteLine("Необходимо ввести трёхзначное число");
+}
+else
+{
+    while(number > 99)
+    {
+        number = number / 10;
+    }
+    Console.WriteLine($"Вторая цифра числа: {number % 10}");
+}
