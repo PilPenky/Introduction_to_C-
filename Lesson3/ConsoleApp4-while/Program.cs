60d0e9a3d42fd7fb81c50eed14c555b9e6d40371
Artur Pilipenko <apg-a@mail.ru>
2022-12-22 21:13:35 +0200

    Задача 4 на уроке - цикл while

---
+++ Lesson3/ConsoleApp4-while/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Пример цикла while */
+
+
+Console.WriteLine("Сколько раз вывести слово ''Привет''? ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+int x = 1;
+
+while (x <= count)
+{
+    if (x == 15) //Пример завершения приложение при определенном значении
+    {
+        break;
+    }
+    Console.WriteLine($"Привет! {x}");
+    x++;
+}
