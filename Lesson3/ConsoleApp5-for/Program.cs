c7e960a2c7df3a7419f2e7d4e72a0abea1cf5375
Artur Pilipenko <apg-a@mail.ru>
2022-12-22 21:13:58 +0200

    Задача 4 на уроке - цикл for

---
+++ Lesson3/ConsoleApp5-for/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Пример цикла for */
+
+Console.WriteLine("Сколько раз вывести слово ''Привет''?");
+int count = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Цикл завершил работу");
+for (int x = 1; x <= count; x++)
+{
+    Console.WriteLine($"Привет! {x}");
+}
