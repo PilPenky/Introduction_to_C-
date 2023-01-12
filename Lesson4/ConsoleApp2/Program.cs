6fe718297c28f871dedce2b5ca886f28a286416d
Artur Pilipenko <apg-a@mail.ru>
2023-01-12 21:14:10 +0200

    Задание2

---
+++ Lesson4/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход число и 
+выдаёт количество цифр в числе. */
+
+int ReadNumber (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountNumber (int a)
+{
+    int count = 0;
+    while (Math.Abs(a) != 0)
+    {
+       a = a/10;
+       count++;
+    } 
+    return count;
+}
+
+int number = ReadNumber("Введите число: ");
+
+int count1 = CountNumber(number);
+Console.WriteLine (count1);
