30fccdfa3f9420d960088a4d1ce7cd8d7822776d
Artur Pilipenko <apg-a@mail.ru>
2023-01-16 21:20:15 +0200

    Замена элементов массива: положительные элементы - отрицательные, и наоборот

---
+++ Lesson5/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*  Напишите программу замена элементов массива: 
+положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] */
+
+int[] GetRamdomArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(leftRange, rightRange+1);
+    }
+    return array;
+}
+
+int[] massive = GetRamdomArray(6, -10, 10);
+Console.WriteLine(string.Join(", ", massive));
+
+for(int i = 0; i < massive.Length; i++)
+{
+    massive[i] = massive[i] * -1;
+}
+
+Console.WriteLine(string.Join(", ", massive));
