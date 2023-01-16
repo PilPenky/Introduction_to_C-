ca48341ebae8acc971b6daa5240f2c4bb205fefa
Artur Pilipenko <apg-a@mail.ru>
2023-01-16 21:22:18 +0200

    Произведение пар чисел в одномерном массиве

---
+++ Lesson5/ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21 */
+
+int[] array = {1,2,3,4,5};
+int size = (array.Length + 1) / 2;
+
+int[] newArray = new int[size];
+for(int i = 0; i < array.Length / 2; i++)
+{
+    newArray[i] = array[i] * array[array.Length - 1 - i];
+}
+
+if(array.Length % 2 == 1)
+{
+    newArray[size - 1] = array[array.Length / 2];
+}
+Console.WriteLine(string.Join(", ", newArray));
