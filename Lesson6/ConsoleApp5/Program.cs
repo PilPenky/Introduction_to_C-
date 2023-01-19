abd1a80ab6512451c330f91d719fd9bb88cb7663
Artur Pilipenko <apg-a@mail.ru>
2023-01-19 21:30:35 +0200

    Программа, которая которая будет создавать копию заданного массива с помощью поэлементного копирования

---
+++ Lesson6/ConsoleApp5/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] GetRandomArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(leftRange, rightRange + 1);
+    }
+
+    return array;
+}
+
+const int size = 5;
+const int leftRange = -9;
+const int rightRange = 9;
+int[] massive = GetRandomArray(size, leftRange, rightRange);
+
+int[] newArray = new int[massive.Length];
+
+for(int i = 0; i < massive.Length; i++)
+{
+    newArray[i] = massive[i];
+}
+
+massive[0] = -1000;
+Console.WriteLine(string.Join(", ", massive));
+Console.WriteLine(string.Join(", ", newArray));
+
