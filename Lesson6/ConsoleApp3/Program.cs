fe4b37e5cf76eef7466e05149c9b025ed6350f1e
Artur Pilipenko <apg-a@mail.ru>
2023-01-19 21:29:24 +0200

    Не используя рекурсию, выводит первые N чисел Фибоначчи

---
+++ Lesson6/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8 */
+
+/* Формула:
+f(n) = f(n-1) + f(n-2) */
+
+int[] Febonachi (int size) 
+{
+
+int[] Arr = new int[size];
+Arr[0] = 0;
+Arr[1] = 1;
+
+for (int i = 2; i < size; i++ ) 
+{
+    
+Arr[i] = Arr[i - 1] + Arr[i - 2];
+
+}
+return Arr;
+}
+int [] massive = Febonachi(10);
+Console.WriteLine(string.Join(" ,", massive ));
