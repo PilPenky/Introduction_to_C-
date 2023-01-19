46a36eedaafc5e131406c3ee22cb2f41da9973f6
Artur Pilipenko <apg-a@mail.ru>
2023-01-19 21:30:00 +0200

    Программа, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины

---
+++ Lesson6/ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон. */
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+bool Treangle(int x, int y, int z) 
+{
+
+    if ( (x + y > z) && (x + z > y) && (z + y > x)){
+return true;
+    }
+
+
+
+    return false;
+}
+
+Console.WriteLine($"Такой треуголник существует: {Treangle(a, b, c)}");
