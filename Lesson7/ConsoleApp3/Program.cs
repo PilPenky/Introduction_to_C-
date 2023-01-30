766810ebe121eb6408174e31f8b50d05b55011cf
Artur Pilipenko <apg-a@mail.ru>
2023-01-30 22:18:34 +0200

    Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты

---
+++ Lesson7/ConsoleApp3/Program.cs
@@ -0,0 +1,73 @@
+﻿/* Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+и замените эти элементы на их квадраты(в примере подсчет индексов начинается с 1, как в математике).
+Например, изначально массив
+выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+​
+Новый массив будет выглядеть
+вот так:
+1 4 7 2
+5 81 2 9
+8 4 2 4 */
+
+//                        строки     столбцы
+int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rows,columns];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Поиск чётных индексов и возведение их квадрат:
+void SquaringMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int tempI = i % 2;
+            int tempJ = j % 2;
+            if (tempI == 0 && tempJ == 0)
+            {
+                Console.Write(matrix[i, j] * matrix[i, j] + " ");
+            }
+            else
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+const int ROWS = 5;
+const int COLUMNS = 5;
+const int LEFTRANGE = 0;
+const int RIGHTRANGE = 10;
+
+int[,] array = GetRandomMatrix(ROWS, COLUMNS, LEFTRANGE, RIGHTRANGE);
+PrintMatrix(array);
+Console.WriteLine();
+SquaringMatrix(array);
