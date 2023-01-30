4076e6a8ce64af50c41e4a4502ac4a4f8f149a57
Artur Pilipenko <apg-a@mail.ru>
2023-01-30 22:24:45 +0200

    Рекурсия. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр

---
+++ Lesson9/ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9 */
+
+int Number1234(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumN (int n) 
+{
+    int temp = 0;
+    if (n == 0) return temp;
+    else 
+    {
+        temp = n % 10 + SumN(n / 10);
+    }
+    return temp;
+}
+
+int number = Number1234("Введите число: ");
+Console.WriteLine(SumN(number));
