11b2f4dd706b927d92288e38e52de45ad12cfda9
Artur Pilipenko <apg-a@mail.ru>
2023-01-30 22:17:18 +0200

    Задайте двумерный массив размером m×n, заполненный случайными целыми числами

---
+++ Lesson7/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1 */
+//                        строки     столбцы
+int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rows,columns];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+const int ROWS = 3;
+const int COLUMNS = 4;
+const int LEFTRANGE = 0;
+const int RIGHTRANGE = 10;
+
+int[,] array = GetRandomMatrix(ROWS, COLUMNS, LEFTRANGE, RIGHTRANGE);
+PrintMatrix(array);
---
+++ Lesson9/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Перевод числа из 10 системы счисления в двоичную:
+
+int number = 13; //Число, которое нужно сконвертировать в двоичное представление
+
+PrintBinaryView(number);
+
+void PrintBinaryView(int n)
+{
+    if(n <= 0) return;
+    PrintBinaryView(n / 2);
+    Console.Write(n % 2);
+}
