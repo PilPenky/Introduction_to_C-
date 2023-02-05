1599548579676d17585a52f3d3626690b0bd1ad2
Artur Pilipenko <apg-a@mail.ru>
2023-02-05 13:50:22 +0200

    ДЗ_3

--- Homework/Homework_Lesson9/Exercise3/Program.cs
+++ Homework/Homework_Lesson9/Exercise3/Program.cs
@@ -7,9 +7,10 @@ int n = 3;
 int RecAcFunction(int M, int N) 
 {
     int ackFunction = 0;
-    if (M == 0) ackFunction = N + 1;
-    else    if (N == 0) ackFunction = RecAcFunction(M-1, 1); 
-            if (M > 0 && N > 0) ackFunction = RecAcFunction(M-1, RecAcFunction(M, N - 1)); 
-            return ackFunction;
+    if (M < 0 || N < 0) Console.Write("Введено отрицательное число. Значение ");
+    else    if (M == 0) ackFunction = N + 1;
+            else    if (N == 0) ackFunction = RecAcFunction(M-1, 1); 
+                    if (M > 0 && N > 0) ackFunction = RecAcFunction(M-1, RecAcFunction(M, N - 1)); 
+                    return ackFunction;
 }
 Console.WriteLine($"{RecAcFunction(m, n)}");
